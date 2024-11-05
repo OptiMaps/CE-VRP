9bd533ff9f4d0c0aca310695916d235f9c312b68
falconlee236 <falconlee236@cau.ac.kr>
2024-11-05 18:02:15 +0900

    update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,15 @@
 # restart ssh service
 /bin/bash -c "sudo service ssh start > /dev/null"
 
-/bin/bash -c "git clone https://github.com/OptiMaps/TrainRepo $HOME/workspace/machine-learning/TrainRepo"
+/bin/bash -c "git clone https://github.com/OptiMaps/TrainRepo $HOME/workspace/graph-mamba/TrainRepo"
 
 /bin/bash -c "mv -f TrainRepo/* ."
 
 /bin/bash -c "rm -rf TrainRepo"
 
+/bin/bash
+
 # execute jupyter lab server
-/bin/bash -c "cd $HOME/workspace/machine-learning && \
-    poetry run jupyter lab --ip 0.0.0.0 --allow-root \
-    --NotebookApp.token= --no-browser --notebook-dir=$HOME"
+# /bin/bash -c "cd $HOME/workspace/graph-mamba && \
+#     poetry run jupyter lab --ip 0.0.0.0 --allow-root \
+#     --NotebookApp.token= --no-browser --notebook-dir=$HOME"
